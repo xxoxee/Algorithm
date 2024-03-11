1660629824f37a0e7629a106c9c6bce0a498d4ad
xxoxee <77658108+xxoxee@users.noreply.github.com>
2024-03-11 20:41:18 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -11,20 +11,17 @@ public class Main {
 		int result = 0;
 		
 		List<Integer> list = new ArrayList<>();
-		list.add(0);
-		list.add(1);
-		
+			
 		for(int i=0; i<=n; i++) {
 			if(i==0) {
-				result = 0;
+				list.add(0);
 			} else if(i==1) {
-				result = 1;
+				list.add(1);
 			} else {
-				result = list.get(i-1) + list.get(i-2);
-				list.add(result);
+				list.add(list.get(i-1) + list.get(i-2));
 			}
 		}
 			
-		System.out.println(result);
+		System.out.println(list.get(n));
 	}
 }
