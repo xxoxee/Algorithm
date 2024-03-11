bdd7ecee43552568357cd6c14540134a872f242b
xxoxee <77658108+xxoxee@users.noreply.github.com>
2024-03-11 20:44:57 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 204 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,20 +6,12 @@ public class Main {
 		int n = sc.nextInt();
 		
 		//----
-		int result = 0;
-		
-		List<Integer> list = new ArrayList<>();
-			
-		for(int i=0; i<=n; i++) {
-			if(i==0) {
-				list.add(0);
-			} else if(i==1) {
-				list.add(1);
-			} else {
-				list.add(list.get(i-1) + list.get(i-2));
-			}
-		}
-			
-		System.out.println(list.get(n));
+		System.out.println(fibonacci(n));
+	}
+	
+	public static int fibonacci(int n) {
+		if(n==0) return 0;
+		if(n==1) return 1;
+		return fibonacci(n-1) + fibonacci(n-2);
 	}
 }
