e235c526652da446a6e3da2150ba5646ab3d2069
xxoxee <77658108+xxoxee@users.noreply.github.com>
2024-06-06 18:09:34 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 13308 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-
-	static int[][] arr;
-	static boolean[] visited;
-	static int N;
 	static int count;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
+		int N = sc.nextInt();
 		int row = sc.nextInt();
 		
 		//1. init
-		arr = new int[N][N];
-		visited = new boolean[N];
+		int[][] arr = new int[N][N];
+		boolean[] visited = new boolean[N];
 		
 		for(int i=0; i<row; i++) {
 			int a = sc.nextInt()-1;
@@ -25,20 +21,20 @@ public class Main {
 		}
 		
 		//2. call dfs
-		dfs(0);
+		dfs(arr, visited, 0);
 		
 		//3. result
 		System.out.println(count-1);
 		
 	}
 
-	public static void dfs(int i) {
+	public static void dfs(int[][] arr, boolean[] visited, int i) {
 		visited[i] = true;
 		count += 1;
 		
-		for(int k=0; k<N; k++) {
+		for(int k=0; k<arr.length; k++) {
 			if(arr[i][k]==1 && !visited[k]) {
-				dfs(k);
+				dfs(arr, visited, k);
 			}
 		}
 	}
