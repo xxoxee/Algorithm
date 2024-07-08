874b9eba0590fc82466f03add86391d281fb86c0
xxoxee <77658108+xxoxee@users.noreply.github.com>
2024-07-08 21:38:46 +0900

    [Silver II] Title: 침투, Time: 736 ms, Memory: 93352 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.java
+++ 백준/Silver/13565. 침투/침투.java
@@ -6,8 +6,11 @@ public class Main {
 	static int M, N;
 	static int[][] board;
 	static boolean[][] visited;
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {1,-1,0,0};
+	
+	static int[] dr = {0,0,-1,1};
+	static int[] dc = {1,-1,0,0};
+	
+	static boolean check = false;
 	
 	public static void main(String[] args) throws Exception{
 
@@ -28,40 +31,34 @@ public class Main {
 		}
 		
 		// 2. dfs
-		boolean flag = false;
 		for(int i=0; i<N; i++) {
 			if(board[0][i]==0) {
-				if(dfs(0, i)) {
-					flag = true;
-					break;
-				}
+				dfs(0, i); 
 			}
+			if(check) break;
 		}
 		
 		// 3. print result
-		System.out.println(flag?"YES":"NO");
+		System.out.println(check ? "YES":"NO");
 	}
 
-	public static boolean dfs(int m, int n) {
-		boolean result = false;
-		
+	public static void dfs(int m, int n) {
 		visited[m][n] = true;
-		if(m==M-1) return true; //안쪽까지 침투됨
 		
+		if(m==M-1) { //안쪽까지 침투됨 -> 종료
+			check = true;
+			return;
+		}
 		
 		for(int i=0; i<4; i++) {
-			int nx = m + dx[i];
-			int ny = n + dy[i];
+			int nr = m + dr[i];
+			int nc = n + dc[i];
 			
-			if(nx>=0 && nx<M && ny>=0 && ny<N) {
-				if(board[nx][ny]==0 && !visited[nx][ny]) { //이동 가능
-					if(dfs(nx, ny)) {
-						result = true;
-						break;
-					}
+			if(nr>=0 && nr<M && nc>=0 && nc<N) {
+				if(board[nr][nc]==0 && !visited[nr][nc]) { //이동가능 여부 확인
+					dfs(nr, nc);
 				}
 			}
 		}
-		return result;
 	}
 }
